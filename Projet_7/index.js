a0d90f40bd0a49f21d3fdbf64c285ebb47d52b6d
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-12-29 19:03:01 +0100

    projet 7 update

--- Projet_7/index.js
+++ Projet_7/index.js
@@ -1,95 +1,45 @@
-const form = document.querySelector("form");
-const input = document.querySelector(".form-input");
-const list = document.querySelector(".list");
-
-// let data = [];
-// let i = 0;
+const form = document.querySelector('form');
+const input = document.querySelector('.form-input');
+const list = document.querySelector('.list');
 
 function storeList() {
-  window.localStorage.todoList = list.innerHTML;
+	window.localStorage.todoList = list.innerHTML;
 }
 
 function getTodos() {
-  if (window.localStorage.todoList) {
-    list.innerHTML = window.localStorage.todoList;
-  } else {
-    list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
-    document.querySelector("li").classList.add("test");
-  }
+	if (window.localStorage.todoList) {
+		list.innerHTML = window.localStorage.todoList;
+	} else {
+		list.innerHTML = `<li>Cliquez sur une tâche pour la supprimer</li>`;
+		document.querySelector('li').classList.add('test');
+	}
 }
 
-// function storeData() {
-//   let todoData = [];
-//   window.localStorage.todoData = data;
-//   window.localStorage.totoI = i;
-// }
+window.addEventListener('load', getTodos());
 
-// function loadData() {
-//   let x = 0;
-//   if (window.localStorage.todoData) {
-//     data = [window.localStorage.todoData];
-//     i = window.localStorage.totoI;
-//   } else {
-//     let data = [];
-//     let i = 0;
-//   }
-// }
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
 
-window.addEventListener("load", getTodos());
-// window.addEventListener("load", loadData());
+	let todo = input.value;
+	input.value = '';
+	const node = document.createElement('li');
+	let textNode = document.createTextNode(todo);
+	node.appendChild(textNode);
+	list.appendChild(node);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let todo = input.value;
-  input.value = "";
-  const node = document.createElement("li");
-  let textNode = document.createTextNode(todo);
-  node.appendChild(textNode);
-  // node.id = i;
-  list.appendChild(node);
-
-  const newLi = [node.textContent, false];
-  data.push(newLi);
-  // i++;
-  storeList();
-  // storeData();
+	const newLi = [node.textContent, false];
+	data.push(newLi);
+	storeList();
 });
 
-list.addEventListener("click", (e) => {
-  let n = e.target.id;
+list.addEventListener('click', (e) => {
+	let n = e.target.id;
 
-  if (e.target.classList.contains("checked")) {
-    e.target.remove();
-    storeList();
-    // storeData();
-  } else {
-    e.target.classList.add("checked");
-    storeList();
-    // storeData();
-  }
+	if (e.target.classList.contains('checked')) {
+		e.target.remove();
+		storeList();
+	} else {
+		e.target.classList.add('checked');
+		storeList();
+	}
 });
-
-// list.addEventListener("click", (e) => {
-//   let n = e.target.id;
-
-//   if (e.target.classList.contains("test")) {
-//     if (e.target.classList.contains("checked")) {
-//       e.target.remove();
-//       storeList();
-//       storeData();
-//     } else {
-//       e.target.classList.add("checked");
-//       storeList();
-//       storeData();
-//     }
-//   } else if (data[e.target.id][1] === true) {
-//     e.target.remove();
-//     storeList();
-//   } else if (e.target && e.target.nodeName == "LI") {
-//     e.target.classList.add("checked");
-//     data[e.target.id][1] = true;
-//     storeList();
-//     storeData();
-//   }
-// });
